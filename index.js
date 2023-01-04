0de3e7ae7bf35b25e154644d2e25db840432a3dc
Sjoerd de Vries <info@supersjoerd.nl>
2023-01-04 20:51:57 +0100

    front-end toegevoegd

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-const scrapeIt = require('scrape-it');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/database.sqlite');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const wait = (time) => new Promise(resolve => setTimeout(resolve, time));
+app.use(express.static('public'));
 
-const scrapeAmazonPage = (asin) => {
-    const url = `https://www.amazon.nl/dp/${asin}`;
-    console.log('Scraping URL', url);
-    return scrapeIt(url, {
-        price: {
-            selector: '.apexPriceToPay>span',
-            convert: p => parseFloat((p.split('€')[1]).replace(',','.'))
-        }
+app.get('/get-data', (req, res) => {
+    db.all('SELECT i.name, ip.price, ip.created_at FROM itemprices ip INNER JOIN items i ON i.id = ip.itemsid', [], async (err, items) => {
+        res.json(items);
     });
-}
+});
 
-const updatePrice = (itemId, price) => {
-    console.log(`Inserting item:`, itemId, price);
-    db.run(`INSERT INTO itemprices (itemsId, price) VALUES ('${itemId}', '${price}');`, (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-}
-
-const scrape = async () => {
-    db.all('SELECT * FROM items', [], async (err, items) => {
-        for (const item of items) {
-            const scraped = await scrapeAmazonPage(item.asin);
-            if (scraped.response.statusCode === 200 && scraped.data.price) {
-                updatePrice(item.id, scraped.data.price);
-            } else {
-                console.log('Out of stock');
-            }
-            await wait(2000);
-        }
-    });
-}
-scrape();
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
